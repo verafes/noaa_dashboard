fcc7cb19894f310ae4099fb10db4059075ccf022
Vera Fesianava <verafes@gmail.com>
2025-07-03 19:08:51 -0700

    Improve app config, dashboard defaults & data setup

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ from app.dashboard import app
 if __name__ == '__main__':
     if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
         print("Running locally... at http://localhost:8050/")
+    debug_mode=os.getenv('FLASK_DEBUG', 'true').lower() == 'true'
     app.run(
         host='0.0.0.0',
         port=8050,
-        debug=True,
+        debug=debug_mode,
     )
